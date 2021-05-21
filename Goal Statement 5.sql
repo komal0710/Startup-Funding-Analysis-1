3fec002ab714f589db4787354cefa14cb3df68f1
KOMAL <komal16dhyani@gmail.com>
2021-05-21 18:24:45 +0530

    Komal Dhyani

--- Goal Statement 5.sql
+++ Goal Statement 5.sql
@@ -22,27 +22,25 @@
   where InvestmentType like  'Angel%'
 
 -- Invested in Maximum No. of Startup
- 
-  select top(10) Investors_Name,count(Startup_Name) as Startup_InvestedIn from dbo.startup_funding
-  where City_Location NOT IN ('Bangalore/Palo Alto','Bangalore/San Mateo','Bangalore/USA','Bangalore/SFO','Bangalore/Bangkok',
-  'Boston','Bumsville','California','Chennai/Singapore','Dallas/Hyderabad','Delhi& Cambridge','Gurgaon/SFO',
-  'Hyderabad/USA','India/Singapore','India/US','London','Menlo Park','Mumbai/Global','Mumbai/NY','Mumbai/UK',
-  'Nairobi','New Delhi/California','New Delhi/US','New Delhi/Houston','Pune/Dubai','New York','New York, 
-  Bengaluru','New York/India','Noida/Singapore','Palo Alto','Pune/Dubai','Pune/Singapore','Pune/US','Pune/Seattle',
-  'San Francisco','San Jose','Santa Monica','Seattle/Banglore','SFO/Banglore','Singapore','Tulangan','USA','US') 
-  group by Investors_Name,InvestmentType
-  having InvestmentType like 'Angel%' and Investors_Name is not Null  order by Startup_InvestedIn desc
 
+   Declare @tablevariable Table(location varchar(5000))
+   insert into @tablevariable values('Bangalore/Palo Alto'),('Bangalore/San Mateo'),('Bangalore/USA'),
+                                    ('Bangalore/SFO'),('Bangalore/Bangkok'),('Boston'),('Bumsville'),
+									('California'),('Chennai/Singapore'),('Dallas/Hyderabad'),
+									('Delhi& Cambridge'),('Gurgaon/SFO'),('Hyderabad/USA'),
+									('India/Singapore'),('India/US'),('London'),('Menlo Park'),
+									('Mumbai/Global'),('Mumbai/NY'),('Mumbai/UK'),('Nairobi'),
+									('New Delhi/California'),('New Delhi/US'),('New Delhi/Houston'),
+									('Pune/Dubai'),('New York'),('New York, Bengaluru'),('New York/India'),
+									('Noida/Singapore'),('Palo Alto'),('Pune/Dubai'),('Pune/Singapore'),
+									('Pune/US'),('Pune/Seattle'),('San Francisco'),('San Jose'),('Santa Monica'),
+									('Seattle/Banglore'),('SFO/Banglore'),('Singapore'),('Tulangan'),('USA'),('US')
  
---Maximum Amount Invested
-  
-  select top(10) Investors_Name,Amount_in_USD from dbo.startup_funding
-  where City_Location NOT IN ('Bangalore/Palo Alto','Bangalore/San Mateo','Bangalore/USA','Bangalore/SFO','Bangalore/Bangkok',
-  'Boston','Bumsville','California','Chennai/Singapore','Dallas/Hyderabad','Delhi& Cambridge','Gurgaon/SFO',
-  'Hyderabad/USA','India/Singapore','India/US','London','Menlo Park','Mumbai/Global','Mumbai/NY','Mumbai/UK',
-  'Nairobi','New Delhi/California','New Delhi/US','New Delhi/Houston','Pune/Dubai','New York','New York, 
-  Bengaluru','New York/India','Noida/Singapore','Palo Alto','Pune/Dubai','Pune/Singapore','Pune/US','Pune/Seattle',
-  'San Francisco','San Jose','Santa Monica','Seattle/Banglore','SFO/Banglore','Singapore','Tulangan','USA','US')
-  group by Investors_Name,InvestmentType,City_Location,Amount_in_USD
-  having InvestmentType like 'Angel%'
-  order by Amount_in_USD desc
+  select Investors_Name,count(Startup_Name) as Startup_InvestedIn from dbo.startup_funding
+  where City_Location NOT IN (Select * from @tablevariable) 
+  group by Investors_Name,InvestmentType
+  having InvestmentType like 'Angel%' and Investors_Name is not Null  
+  order by Startup_InvestedIn desc
+  offset 1 row
+  fetch next 5 rows only
+  go  
