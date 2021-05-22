18563e7eb14fbf313e93cbcb6a0687faeeef7dbd
komal0710 <83998407+komal0710@users.noreply.github.com>
2021-05-22 12:40:47 +0530

    Update Goal Statement 1.sql

--- Goal Statement 1.sql
+++ Goal Statement 1.sql
@@ -1,9 +1,5 @@
- select DISTINCT Investors_Name,count(Startup_Name) as startup_count, SUM(Amount_in_USD) AS Total_Amount from dbo.startup_funding
-  WHERE City_Location NOT IN ('Bangalore/Palo Alto','Bangalore/San Mateo','Bangalore/USA','Bangalore/SFO','Bangalore/Bangkok',
-  'Boston','Bumsville','California','Chennai/Singapore','Dallas/Hyderabad','Delhi& Cambridge','Gurgaon/SFO',
-  'Hyderabad/USA','India/Singapore','India/US','London','Menlo Park','Mumbai/Global','Mumbai/NY','Mumbai/UK',
-  'Nairobi','New Delhi/California','New Delhi/US','New Delhi/Houston','Pune/Dubai','New York','New York, 
-  Bengaluru','New York/India','Noida/Singapore','Palo Alto','Pune/Dubai','Pune/Singapore','Pune/US','Pune/Seattle',
-  'San Francisco','San Jose','Santa Monica','Seattle/Banglore','SFO/Banglore','Singapore','Tulangan','USA','US')
-  group by Investors_Name
+SELECT top(5) Investors_Name,SUM(Amount_in_USD) AS Total_Amount
+ FROM dbo.startup_funding
+ WHERE City_Location NOT IN (Select * from [dbo].[City_locations])
+ GROUP BY Investors_Name having Investors_Name not in ('NULL')
   ORDER BY Total_Amount DESC
