1415d22ee93827bcc44ff7716e33de8a82dad12e
komal0710 <83998407+komal0710@users.noreply.github.com>
2021-05-22 16:26:45 +0530

    Update Goal Statement 1.sql

--- Goal Statement 1.sql
+++ Goal Statement 1.sql
@@ -1,10 +1,20 @@
- select DISTINCT Investors_Name,count(Startup_Name) as startup_count, SUM(Amount_in_USD) AS Total_Amount from dbo.startup_funding
-  WHERE City_Location NOT IN ('Bangalore/Palo Alto','Bangalore/San Mateo','Bangalore/USA','Bangalore/SFO','Bangalore/Bangkok',
-  'Boston','Bumsville','California','Chennai/Singapore','Dallas/Hyderabad','Delhi& Cambridge','Gurgaon/SFO',
-  'Hyderabad/USA','India/Singapore','India/US','London','Menlo Park','Mumbai/Global','Mumbai/NY','Mumbai/UK',
-  'Nairobi','New Delhi/California','New Delhi/US','New Delhi/Houston','Pune/Dubai','New York','New York, 
-  Bengaluru','New York/India','Noida/Singapore','Palo Alto','Pune/Dubai','Pune/Singapore','Pune/US','Pune/Seattle',
-  'San Francisco','San Jose','Santa Monica','Seattle/Banglore','SFO/Banglore','Singapore','Tulangan','USA','US')
-  group by Investors_Name
-  ORDER BY Total_Amount DESC
+ Declare @tablevariable Table(location varchar(5000))
+   insert into @tablevariable values('Bangalore/Palo Alto'),('Bangalore/San Mateo'),('Bangalore/USA'),
+                                    ('Bangalore/SFO'),('Bangalore/Bangkok'),('Boston'),('Bumsville'),
+									('California'),('Chennai/Singapore'),('Dallas/Hyderabad'),
+									('Delhi& Cambridge'),('Gurgaon/SFO'),('Hyderabad/USA'),
+									('India/Singapore'),('India/US'),('London'),('Menlo Park'),
+									('Mumbai/Global'),('Mumbai/NY'),('Mumbai/UK'),('Nairobi'),
+									('New Delhi/California'),('New Delhi/US'),('New Delhi/Houston'),
+									('Pune/Dubai'),('New York'),('New York, Bengaluru'),('New York/India'),
+									('Noida/Singapore'),('Palo Alto'),('Pune/Dubai'),('Pune/Singapore'),
+									('Pune/US'),('Pune/Seattle'),('San Francisco'),('San Jose'),('Santa Monica'),
+									('Seattle/Banglore'),('SFO/Banglore'),('Singapore'),('Tulangan'),('USA'),('US')
+
+
+ SELECT TOP(5) Investors_Name,SUM(Amount_in_USD) AS Total_Amount
+ FROM dbo.startup_funding
+ WHERE City_Location NOT IN (Select * from @tablevariable)
+ GROUP BY Investors_Name having Investors_Name not in ('NULL')
+ ORDER BY Total_Amount DESC
   
